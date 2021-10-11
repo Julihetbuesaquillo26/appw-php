0686e4bb5f734e715621ff3db617c6b2e78fb315
julieta B <jsbt.1020@gmail.com>
2021-10-11 17:52:25 -0500

    se creo un index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php>
+//Comentarios libres
+
+$name = "julieta"; //(Cuando los datos son tipo string se puede usar comillas dobles) STRING
+$lastname = "Buesaquillo"; //STRING
+$age = "26"; //NUMBER
+$hobbies = [
+
+    "Monday" => "read",
+    "sunday" => "play the guitar"
+];
+
+//Para imprimir en pantalla se usa el elemento ECHO
+
+?>
